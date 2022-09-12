3a6db737bc280d8b050ae27cb8b5ae462696c08f
lukeyu1025 <74660025+lukeyu1025@users.noreply.github.com>
2022-09-12 14:36:45 +0800

    Update 2022_05_Problem02.cpp
    fix: TLE time limit exceeded

--- 2022_05_Problem02.cpp
+++ 2022_05_Problem02.cpp
@@ -5,12 +5,12 @@ int main() {
    cin>>n;
    for(int i=0;i<n;i++){
        cin>>m;
-       for(;ans%m!=0;temp++){
+       for(;(ans%=m)!=0;temp++){
             ans=10*ans+1;
        }
        cout<<temp<<endl;
        ans=1;
        temp=1;
    }
-   return 0;S
+   return 0;
 }  
