9b73a83c4f6090911dbbaaf731b60a383dccd4a3
lukeyu1025 <74660025+lukeyu1025@users.noreply.github.com>
2022-09-12 14:39:12 +0800

    Update 2022_05_Problem02.cpp
    different way

--- 2022_05_Problem02.cpp
+++ 2022_05_Problem02.cpp
@@ -1,16 +1,17 @@
 #include <iostream>  
 using namespace std;  
 int main() {  
-   int n,m,temp=1,ans=1;
+   int n,m;
    cin>>n;
    for(int i=0;i<n;i++){
+       int temp=1,ans=1;
        cin>>m;
-       for(;(ans%=m)!=0;temp++){
-            ans=10*ans+1;
+       while((ans%=m)!=0){
+           ans*=10;
+           ans++;
+           temp++;
        }
        cout<<temp<<endl;
-       ans=1;
-       temp=1;
    }
    return 0;
 }  
