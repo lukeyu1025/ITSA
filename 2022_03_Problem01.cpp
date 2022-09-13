27c175461ce5e3554e377972163ed5fd10753e07
lukeyu1025 <74660025+lukeyu1025@users.noreply.github.com>
2022-09-13 14:57:35 +0800

    Update 2022_03_Problem01.cpp

--- 2022_03_Problem01.cpp
+++ 2022_03_Problem01.cpp
@@ -9,16 +9,36 @@ int main(){
         string str;//48~57
         getline(cin,str);
         int len=str.length();
+        bool flag=true;
         for(int j=0;j<len;j+=2){
-            if((int)str[j]>=48&&(int)str[j]<=57)cout<<str[j];
-            else if(str[j]=='*'||str[j]=='/'||str[j]=='(')op.push(str[j]);
+            if((int)str[j]>=48&&(int)str[j]<=57){
+                if(flag==true){
+                    cout<<str[j];
+                    flag=false;
+                }
+                else{
+                    cout<<" "<<str[j];
+                }
+            }
+            else if(str[j]=='*'||str[j]=='/'){
+                if(op.empty())op.push(str[j]);
+                else{
+                        if(op.top()=='*'||op.top()=='/'){
+                            char temp=op.top();
+                            op.pop();
+                            cout<<" "<<temp;
+                        }
+                    op.push(str[j]);
+                }
+                }
+            else if(str[j]=='(')op.push(str[j]);
             else if(str[j]=='+'||str[j]=='-'){
                 if(op.empty())op.push(str[j]);
                 else{
                         if(op.top()=='*'||op.top()=='/'){
                             char temp=op.top();
                             op.pop();
-                            cout<<temp;
+                            cout<<" "<<temp;
                         }
                     op.push(str[j]);
                 }
@@ -26,7 +46,7 @@ int main(){
             else if(str[j]==')'){
                 while(op.top()!='('){
                     char temp=op.top();
-                    cout<<temp;
+                    cout<<" "<<temp;
                     op.pop();
                 }
                 op.pop();
@@ -35,7 +55,7 @@ int main(){
         while(!op.empty()){
             char temp=op.top();
             op.pop();
-            cout<<temp;
+            cout<<" "<<temp;
         }
         cout<<endl;
     }
