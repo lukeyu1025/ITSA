b02d8cc1a6c451eca619770cb6f9c449be337694
lukeyu1025 <74660025+lukeyu1025@users.noreply.github.com>
2022-09-14 19:32:19 +0800

    Update 2021_12_Problem03.cpp

--- 2021_12_Problem03.cpp
+++ 2021_12_Problem03.cpp
@@ -6,17 +6,18 @@ int main(){
    for(int i=0;i<num;i++){
     int cnt,rug[100001],len=0,max=0;
     cin>>cnt;
+    for(int j=0;j<=100000;j++){rug[j]=0;}
     for(int j=0;j<cnt;j++){
         int start,end;
         cin>>start>>end;
         if(end>max)max=end;
-        for(int k=start;k<=end;k++){
+        for(int k=start;k<end;k++){
             rug[k]=1;
         }
     }
     for(int j=0;j<=max;j++){
         if(rug[j]==1)len++;
     }
-    cout<<len-2<<endl;
+    cout<<len<<endl;
     }
 }
