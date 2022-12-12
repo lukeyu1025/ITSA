c7747ecbe61ba18fde72d428cfb8b69c9bc3c6dd
lukeyu1025 <74660025+lukeyu1025@users.noreply.github.com>
2022-12-12 14:31:55 +0800

    Create 2021_03_Problem01.py

---
+++ 2021_03_Problem01.py
@@ -0,0 +1,11 @@
+import numpy as np
+n=int(input())
+string=input()
+timeline=[0]*24
+string=string.split( )
+string=[int(x) for x in string]
+for i in range(0,len(string),2):
+    for j in range(string[i],string[i+1]+1):
+        timeline[j-1]+=1
+print(max(timeline))
+    
